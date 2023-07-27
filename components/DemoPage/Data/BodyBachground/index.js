9d0520f5dc7d2ae550979cacc2f8c9cf3bdb4dd7
anhtruong1209 <sdtla0911114819@gmail.conm>
2023-07-27 11:56:52 +0700

    add new image

--- components/DemoPage/Data/BodyBachground/index.js
+++ components/DemoPage/Data/BodyBachground/index.js
@@ -1,64 +1,94 @@
 import baseRs from '../../../../utils/baseRs';
 const faceBackgroundLeftData = [
   {
-    src: baseRs + `demo/human/human_01.png`,
+    src: baseRs + `demo/human/000001_0.jpg`,
     altImg: "result2",
     altThumb: "thumbnail2",
   },
   {
-    src: baseRs + `demo/human/human_02.png`,
+    src: baseRs + `demo/human/000010_0.jpg`,
     altImg: "result4",
     altThumb: "thumbnail4",
   },
   {
-    src: baseRs + `demo/human/human_03.jpeg`,
+    src: baseRs + `demo/human/000020_0.jpg`,
     altImg: "result4",
     altThumb: "thumbnail4",
   },
   {
-    src: baseRs + `demo/human/human_04.jpg`,
+    src: baseRs + `demo/human/000028_0.jpg`,
     altImg: "result4",
     altThumb: "thumbnail4",
   },
   {
-    src: baseRs + `demo/human/human_02.png`,
+    src: baseRs + `demo/human/000038_0.jpg`,
     altImg: "result4",
     altThumb: "thumbnail4",
   },
   {
-    src: baseRs + `demo/human/human_03.jpeg`,
+    src: baseRs + `demo/human/000048_0.jpg`,
+    altImg: "result4",
+    altThumb: "thumbnail4",
+  },
+  {
+    src: baseRs + `demo/human/000057_0.jpg`,
+    altImg: "result4",
+    altThumb: "thumbnail4",
+  },
+  {
+    src: baseRs + `demo/human/000066_0.jpg`,
+    altImg: "result4",
+    altThumb: "thumbnail4",
+  },
+  {
+    src: baseRs + `demo/human/000074_0.jpg`,
     altImg: "result4",
     altThumb: "thumbnail4",
   },
 ];
 const faceBackgroundRightData = [
   {
-    src: baseRs + `demo/glass/glasses_01.png`,
+    src: baseRs + `demo/glass/001744_1.jpg`,
     altImg: "result4",
     altThumb: "thumbnail4",
   },
   {
-    src: baseRs + `demo/glass/glasses_02.png`,
+    src: baseRs + `demo/glass/004325_1.jpg`,
     altImg: "result2",
     altThumb: "thumbnail2",
   },
   {
-    src: baseRs + `demo/glass/glasses_03.png`,
+    src: baseRs + `demo/glass/002022_1.jpg`,
     altImg: "result4",
     altThumb: "thumbnail4",
   },
   {
-    src: baseRs + `demo/glass/glasses_01.png`,
+    src: baseRs + `demo/glass/014652_1.jpg`,
     altImg: "result4",
     altThumb: "thumbnail4",
   },
   {
-    src: baseRs + `demo/glass/glasses_02.png`,
+    src: baseRs + `demo/glass/010143_1.jpg`,
     altImg: "result4",
     altThumb: "thumbnail4",
   },
   {
-    src: baseRs + `demo/glass/glasses_03.png`,
+    src: baseRs + `demo/glass/010608_1.jpg`,
+    altImg: "result4",
+    altThumb: "thumbnail4",
+  },
+  {
+    src: baseRs + `demo/glass/012578_1.jpg`,
+    altImg: "result4",
+    altThumb: "thumbnail4",
+  },
+  {
+    src: baseRs + `demo/glass/09595_1.jpg`,
+    altImg: "result4",
+    altThumb: "thumbnail4",
+  },
+  {
+    src: baseRs + `demo/glass/015392_1.jpg`,
     altImg: "result4",
     altThumb: "thumbnail4",
   },
